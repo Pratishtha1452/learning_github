8f6e69b777990a25464fe31ca15a93ab33170add
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-03-22 14:43:01 +0530

    wrote a method in the program

--- Animal.java
+++ Animal.java
@@ -1,5 +1,11 @@
 public class Animal{
     public static void main(String args[]){
         System.out.println("Hello");
+	method1();
+    }
+    public static void method1(){
+	int n = 0;
+	System.out.println(n);
     }
 }
+
