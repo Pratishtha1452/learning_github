d6c652a9cc822cd7d1c11c58552191b678d9b194
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-03-22 15:02:46 +0530

    inheritance-polymorphism

--- file.java
+++ file.java
@@ -1,5 +1,21 @@
 public class Main{
     public static void main(String [] args){
         System.out.println("Hello");
+        one obj = new two();
+        obj.func1();
+    }
+}
+class one{
+    public void func1(){
+        int n = 1;
+        n++;
+        
+    }
+}
+class two extends one{
+    @Override
+    public void func1(){
+        int n = 2;
+        System.out.println(n);
     }
 }
